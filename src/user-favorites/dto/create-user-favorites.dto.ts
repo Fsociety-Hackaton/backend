dacd44f88cafab92fb1a6ae6af60520158864a9e
johan-avila <johan-avila@outlook.com>
2020-12-19 11:38:32 -0600

    Add Swagger

--- src/user-favorites/dto/create-user-favorites.dto.ts
+++ src/user-favorites/dto/create-user-favorites.dto.ts
@@ -1,6 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateUserFavoritesDto {
+    @ApiProperty()
     readonly name: string;
+    @ApiProperty()
     readonly jobId: string;
+    @ApiProperty()
     readonly date: string;
+    @ApiProperty()
     readonly portal: string;
 }
