0c88196d05645a8811681a6a1422e207b5c0ffef
Julian Cubillos <juliandavid25@hotmail.com>
2020-12-19 15:04:14 -0500

    Feat config swagger doc

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,25 +1,32 @@
 import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags, ApiHeader, ApiCreatedResponse } from '@nestjs/swagger';
 import { User } from 'src/common/decorators';
 import { AuthService } from './auth.service';
 
 import { JWTAuthGuard } from './guards/jwtAuth.guard';
 import { LocalAuthGuard } from './guards/localAuth.guard';
 
+@ApiHeader({
+  name: 'Login',
+  description: 'User Login Methods',
+})
+@ApiTags('APP Login Module')
 @Controller('auth')
 export class AuthController {
     constructor(private  authService : AuthService){}
     
     @UseGuards(LocalAuthGuard) 
     @Post("login")
+    @ApiCreatedResponse({ description: 'Login success' })
     async login(@User() user: any){   
         const data = await this.authService.login(user)        
-        return {message:"Login succes"  , data} 
+        return {message:"Login success"  , data} 
     }
 
     @UseGuards(JWTAuthGuard) 
     @ApiBearerAuth()
     @Get("profile")
+    @ApiCreatedResponse({ description: 'Get success' })
     profile(
         @User() user
     ){
--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -2,12 +2,19 @@ import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { get } from 'http';
 import { JWTAuthGuard } from 'src/auth/guards/jwtAuth.guard';
 import { JobsService } from './jobs.service';
+import { ApiTags, ApiHeader, ApiCreatedResponse } from '@nestjs/swagger';
 
+@ApiHeader({
+  name: 'Jobs',
+  description: 'Search Jobs Methods',
+})
+@ApiTags('APP Jobs Module')
 @Controller('jobs')
 export class JobsController {
     constructor(private readonly jobsService: JobsService){}
     
     @Get()
+    @ApiCreatedResponse({ description: 'All Jobs' })
     async getAllJobs(){
         const data = await this.jobsService.getAll([])
         return {
@@ -17,6 +24,7 @@ export class JobsController {
     }
     @UseGuards(JWTAuthGuard)
     @Get(":id")
+    @ApiCreatedResponse({ description: 'One Job' })
     async getOneJob(@Param("id") id: string ){
         const data =  await this.jobsService.getOne(id)
         return {
